808acb2bbcfda2d3b7d274385561228b7c6201f2
Sarwat Irum <sarwatirum14@gmail.com>
2025-07-17 17:06:47 +0500

    Fix: Replaced anchor tags with Link in Footer for Next.js compliance

--- app/components/footer.js
+++ app/components/footer.js
@@ -1,3 +1,7 @@
+'use client';
+
+import Link from "next/link";
+
 export default function Footer() {
     return ( <
         footer className = "bg-black text-white py-10" >
@@ -10,14 +14,14 @@ export default function Footer() {
         h3 className = "text-xl font-semibold mb-4" > Quick Links < /h3> <
         ul className = "space-y-2" >
         <
-        li > < a href = "/"
-        className = "hover:underline" > Home < /a></li >
+        li > < Link href = "/"
+        className = "hover:underline" > Home < /Link></li >
         <
-        li > < a href = "/booking"
-        className = "hover:underline" > Bookings < /a></li >
+        li > < Link href = "/booking"
+        className = "hover:underline" > Bookings < /Link></li >
         <
-        li > < a href = "/contact"
-        className = "hover:underline" > Contact Us < /a></li >
+        li > < Link href = "/contact"
+        className = "hover:underline" > Contact Us < /Link></li >
         <
         /ul> <
         /div>
@@ -28,8 +32,8 @@ export default function Footer() {
         h3 className = "text-xl font-semibold mb-4" > Contact < /h3> <
         ul className = "space-y-2 text-sm" >
         <
-        li > Email: < a href = "mailto:support@shaevents.com"
-        className = "hover:underline" > support @Irfanievents.com < /a></li >
+        li > Email: < a href = "mailto:support@irfanievents.com"
+        className = "hover:underline" > support @irfanievents.com < /a></li >
         <
         li > Phone: < a href = "tel:+1234567890"
         className = "hover:underline" > +123 456 7890 < /a></li >
@@ -46,41 +50,41 @@ export default function Footer() {
         We provide seamless event booking experiences
         for weddings, corporate events, birthdays, and more. <
         /p> <
-        div className = "flex justify-center sm:justify-start space-x-4 mt-2" > { /* Home Icon with Tooltip */ } <
+        div className = "flex justify-center sm:justify-start space-x-4 mt-2" >
+
+        { /* Home Icon */ } <
         div className = "relative group" >
         <
-        a href = "/"
-        className = "hover:text-pink-400" > 🌐 < /a> <
+        Link href = "/"
+        className = "hover:text-pink-400" > 🌐 < /Link> <
         div className = "absolute bottom-full mb-1 left-1/2 -translate-x-1/2 px-2 py-1 text-xs text-white bg-black rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap" >
         Home <
         /div> <
         /div>
 
-        { /* About Us Icon with Tooltip */ } <
+        { /* About Us Icon */ } <
         div className = "relative group" >
         <
-        a href = "/#about"
-        className = "hover:text-pink-400" > 📘 < /a> <
+        Link href = "/#about"
+        className = "hover:text-pink-400" > 📘 < /Link> <
         div className = "absolute bottom-full mb-1 left-1/2 -translate-x-1/2 px-2 py-1 text-xs text-white bg-black rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap" >
         About Us <
         /div> <
         /div>
 
-        { /* Gallery Icon with Tooltip */ } <
+        { /* Gallery Icon */ } <
         div className = "relative group" >
         <
-        a href = "/gallery"
-        className = "hover:text-pink-400" > 📸 < /a> <
+        Link href = "/gallery"
+        className = "hover:text-pink-400" > 📸 < /Link> <
         div className = "absolute bottom-full mb-1 left-1/2 -translate-x-1/2 px-2 py-1 text-xs text-white bg-black rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap" >
         Gallery <
         /div> <
+        /div>
+
+        <
         /div> <
-        /div>
-
-        <
-        /div>
-
-        <
+        /div> <
         /div>
 
         { /* Copyright */ } <
