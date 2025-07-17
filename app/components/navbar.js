043d9589d1ff3adea4c6fa76a649d0e4126a67ed
Sarwat Irum <sarwatirum14@gmail.com>
2025-07-17 16:57:49 +0500

    Fix ESLint: Replaced <a> with <Link> in Navbar

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,20 +1,33 @@
 // app/components/Navbar.js
-'use client'
+'use client';
 import Link from 'next/link';
 
 export default function Navbar() {
-  return (
-    <nav className="fixed top-0 w-full z-50 bg-black/40 text-white">
-      <div className="max-w-7xl mx-auto px-4 py-4 flex justify-end space-x-6">
-        <Link href="/" className="hover:text-pink-500 transition">Home</Link>
-        <a href="/#about" className="hover:text-pink-500 transition">About us</a>
-        <a href="/#events" className="hover:text-pink-500 transition">Event</a>
-        <Link href="/gallery" className="hover:text-pink-500 transition">Gallery</Link>
-        <a href="/contact" className="hover:text-pink-500 transition">Contact Us</a>
-        <a href="/admin" className="hover:text-pink-500 transition">Admin Login</a>
-        <button className="border-2 border-white border-r-4 p-0.5"><a href="/booking" className="hover:text-pink-500 transition">BOOK NOW!</a></button>
-
-      </div>
-    </nav>
-  );
+    return ( <
+        nav className = "fixed top-0 w-full z-50 bg-black/40 text-white" >
+        <
+        div className = "max-w-7xl mx-auto px-4 py-4 flex justify-end space-x-6" >
+        <
+        Link href = "/"
+        className = "hover:text-pink-500 transition" > Home < /Link> <
+        Link href = "/#about"
+        className = "hover:text-pink-500 transition" > About us < /Link> <
+        Link href = "/#events"
+        className = "hover:text-pink-500 transition" > Event < /Link> <
+        Link href = "/gallery"
+        className = "hover:text-pink-500 transition" > Gallery < /Link> <
+        Link href = "/contact"
+        className = "hover:text-pink-500 transition" > Contact Us < /Link> <
+        Link href = "/admin"
+        className = "hover:text-pink-500 transition" > Admin Login < /Link> <
+        Link href = "/booking" >
+        <
+        button className = "border-2 border-white border-r-4 p-0.5 hover:text-pink-500 transition" >
+        BOOK NOW!
+        <
+        /button> <
+        /Link> <
+        /div> <
+        /nav>
+    );
 }
